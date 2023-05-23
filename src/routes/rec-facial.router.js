c324d1ffa709b6fd163551cd1716789fa7eb8235
evalton <evalton.nunes@gmail.com>
2023-05-23 18:07:51 -0300

    configuracao

--- src/routes/rec-facial.router.js
+++ src/routes/rec-facial.router.js
@@ -8,4 +8,12 @@ reconhecimentoFacialRouter.post('/quantidade-rostos',async (req, res)=>{
 
 });
 
+//API TESTES
+reconhecimentoFacialRouter.get('/teste', async (req, res) => {
+  res.writeHeader(200, {"Content-Type": "text/html"});
+  res.write("<h1>Teste Node</h1>");
+  res.end();
+
+});
+
 module.exports = reconhecimentoFacialRouter;
