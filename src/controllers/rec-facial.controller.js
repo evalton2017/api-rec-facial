ceafa7dfc232f79338635607bc9fcd48e778cc7d
evalton <evalton.nunes@gmail.com>
2023-05-23 19:07:44 -0300

    teste projeto

--- src/controllers/rec-facial.controller.js
+++
@@ -1,8 +0,0 @@
-const faceApiService = require('../service/faceapiService');
-
-exports.quantidadeRostos = async (req, res) => {
-  const { file } = req.files;
-  console.log(file)
-  const result = await faceApiService.detect(file.data, file.name);
-  return res.status(200).json(result.length+" Rostos encontrados");
-};
--- src/routes/rec-facial.router.js
+++ src/routes/rec-facial.router.js
@@ -2,11 +2,6 @@ const express = require('express');
 const reconhecimentoFacialRouter = express.Router();
 const controller = require('../controllers/rec-facial.controller')
 
-//ROTA PARA CRIAR USUARIO
-reconhecimentoFacialRouter.post('/quantidade-rostos',async (req, res)=>{
- return  await  controller.quantidadeRostos(req,res)
-
-});
 
 //API TESTES
 reconhecimentoFacialRouter.get('/teste', async (req, res) => {
